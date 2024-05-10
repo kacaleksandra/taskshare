eb34a6db4ebefedf005f4292773f1c906f471566
Aleksandra Kacprzak <elinda100@gmail.com>
2024-05-10 19:22:17 +0200

    success sign-up page & small fix in button component

--- app/(auth)/success/page.tsx
+++ app/(auth)/success/page.tsx
@@ -1,8 +1,24 @@
+'use client';
+
+import { Button } from '@/app/_components/button';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
 const SignUpSuccessPage = () => {
+  const router = useRouter();
   return (
-    <div>
-      <h1>Sign Up Success</h1>
-      <p>You have successfully signed up!</p>
+    <div className='flex flex-col gap-6 h-full items-center justify-center'>
+      <p className='text-lg font-semibold tracking-wide'>
+        You have successfully signed-up!
+      </p>
+      <p>Now, you can login with button below.</p>
+      <Button
+        size='lg'
+        className='my-4 w-1/3'
+        onClick={() => router.push('/sign-in')}
+      >
+        Login
+      </Button>
     </div>
   );
 };
